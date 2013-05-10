22f31a19c021add715a7e0e7c99c8dd04315caf6
Thomas Laemmlein <thomaslaemmlein@gmail.com>
2013-05-10 21:24:07 +0200

    ADD: Bluetooth connection button for connected/not connected state added.

--- src/de/thomaslaemmlein/ttc/MainActivity.java
+++ src/de/thomaslaemmlein/ttc/MainActivity.java
@@ -2,6 +2,7 @@ package de.thomaslaemmlein.ttc;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -9,6 +10,8 @@ import android.os.Bundle;
 import android.text.Html;
 
 public class MainActivity extends SherlockActivity {
+	
+	private boolean m_bBluetoothConnectionStateIcon;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +20,37 @@ public class MainActivity extends SherlockActivity {
 		
 		getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#262626")));
 		getSupportActionBar().setTitle(Html.fromHtml("<font color='#ffffff'>TTC</font>"));
+	
 	}
 	
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		//getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+    	com.actionbarsherlock.view.MenuInflater inflater = getSupportMenuInflater();
+ 	   	inflater.inflate(R.menu.main, (com.actionbarsherlock.view.Menu) menu);    	
+ 	
+ 	   	m_bBluetoothConnectionStateIcon = false;
+
+ 	   	return super.onCreateOptionsMenu(menu);
 	}
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.bluetooth_connection_button:
+            	if ( m_bBluetoothConnectionStateIcon )
+            	{
+            		item.setIcon(R.drawable.bluetooth_not_connected);
+            		m_bBluetoothConnectionStateIcon = false;
+            	}
+            	else
+            	{
+            		item.setIcon(R.drawable.bluetooth_connected);
+            		m_bBluetoothConnectionStateIcon = true;
+            	}
+            	return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }    
 
 }
