9cfe9d10d41e8d9a469fbd0626185cb5e5e275ff
Thomas Laemmlein <thomaslaemmlein@gmail.com>
2013-05-10 20:42:19 +0200

    ADD: Usage of ActionBarSherlock. Background is black.

--- src/de/thomaslaemmlein/ttc/MainActivity.java
+++ src/de/thomaslaemmlein/ttc/MainActivity.java
@@ -1,21 +1,22 @@
 package de.thomaslaemmlein.ttc;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
 
-public class MainActivity extends Activity {
+import android.os.Bundle;
+
+public class MainActivity extends SherlockActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		//getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
